7fe9d2e897ee0a511bd810f40061d703cd443bf3
krypton-dev <krypton-dev>
2016-08-25 22:32:27 +0000

    delete files

--- tests/files/ansible/test-files/create-docker-images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-# creates the necessary docker images to run testrunner.sh locally
-
-docker build --tag="krypton/cppjit-testrunner" docker-cppjit
-docker build --tag="krypton/python-testrunner" docker-python
-docker build --tag="krypton/go-testrunner" docker-go
